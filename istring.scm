4e93ae2101c801d1e6c210191802ba9e891e7b74
Rígille Scherrer Borges Menezes <rigillesbmenezes@protonmail.com>
2021-05-21 16:42:36 -0300

    i fucking love not having types (no i don't)

--- istring.scm
+++ istring.scm
@@ -1,7 +1,7 @@
 (load-shared-object "./libistring.so")
 ; immutable string type
 (define-ftype istring uptr)
-(define string->istring (foreign-procedure "str_to_istr" (string) istring))
+(define string->istring (foreign-procedure "str_to_istr" (ptr) istring))
 (define free-istring (foreign-procedure "free_istr" (istring) void))
 (define foreign-double (foreign-procedure "foreign_double" (integer-64) integer-64))
 (define foreign-length (foreign-procedure "string_test" (ptr) integer-64))
