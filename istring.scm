c5844c721216b4b1a21d13f64d402375fee9413e
Rígille Scherrer Borges Menezes <rigillesbmenezes@protonmail.com>
2021-05-24 16:24:45 -0300

    integrate with scheme's garbage collector

--- istring.scm
+++ istring.scm
@@ -1,17 +1,32 @@
 (load-shared-object "./libistring.so")
+
 ; immutable string type
-(define-ftype istring uptr)
+(define-record istring
+  ((immutable uptr ptr)))
 (define istring-guardian (make-guardian))
 
-(define str_to_istr (foreign-procedure "str_to_istr" (ptr) istring))
-(define string->istring
-  (lambda (x)
-    ; TODO check x is really a string?
-    (let ((ret (str_to_istr x)))
-      (istring-guardian ret)
-      ret)))
-(define free-istring (foreign-procedure "free_istr" (istring) void))
+; foreign-procedures
+(define str_to_istr (foreign-procedure "str_to_istr" (ptr) uptr))
+(define free-istring   (foreign-procedure "free_istr" (uptr) void))
 (define foreign-double (foreign-procedure "foreign_double" (integer-64) integer-64))
 (define foreign-length (foreign-procedure "string_test" (ptr) integer-64))
 
-(do ((i 0 (+ i 1))) ((eq? i 5)) (string->istring "hi"))
+; wrapped procedures
+(define string->istring
+  (lambda (x)
+    ; TODO check x is really a string?
+    (let ((ret (make-istring (str_to_istr x))))
+      (istring-guardian ret)
+      ret)))
+
+(collect-request-handler
+  (lambda ()
+    ; first, invoke the collector
+    (collect)
+    ; then free any storage that has been dropped
+    (let f ()
+      (let ([x (istring-guardian)])
+        (when x
+          (display "freeing istring!")
+          (free-istring (istring-ptr x))
+          (f))))))
